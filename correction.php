21b1ff767c4cb3fdbbfdc51402025e2e64495b93
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-12 17:41:09 +0100

    cambiato nome a variabile

--- correction.php
+++ correction.php
@@ -3,9 +3,9 @@
 <?php 
 $paragrafo = $_GET['paragrafo'];
 $censura = $_GET['badwords'];
-$nuovaParola = echo str_replace($paragrafo,$censura,'***');
+$nuovoParagrafo = str_replace($paragrafo,$censura,'***');
 ?>
 
 <p>
-    Il povero <?php $nuovaParola; ?> non sapeva più a chi rivolgersi, ma continuò a saltellare… Lo vide un abete e gli chiese
+    Il povero <?php $nuovoParagrafo; ?> non sapeva più a chi rivolgersi, ma continuò a saltellare… Lo vide un abete e gli chiese
 </p>
