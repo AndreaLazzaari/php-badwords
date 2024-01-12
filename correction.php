67ad9b2091f8c5ad0e1f52594d812444d57c3fa6
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-12 17:34:19 +0100

    provato a creare una nuova variabile nuovaParola ma non funzionante

--- correction.php
+++ correction.php
@@ -3,9 +3,9 @@
 <?php 
 $paragrafo = $_GET['paragrafo'];
 $censura = $_GET['badwords'];
-
+$nuovaParola = echo str_replace($paragrafo,$censura,'***');
 ?>
 
 <p>
-    Il povero <?php echo str_replace($paragrafo,$censura,'***'); ?> non sapeva più a chi rivolgersi, ma continuò a saltellare… Lo vide un abete e gli chiese
+    Il povero <?php $nuovaParola; ?> non sapeva più a chi rivolgersi, ma continuò a saltellare… Lo vide un abete e gli chiese
 </p>
