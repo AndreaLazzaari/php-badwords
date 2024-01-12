56991e642fbc9f4f94082ae3054338679b5527ea
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-12 17:42:40 +0100

    inserito echo dentro al p

--- correction.php
+++ correction.php
@@ -4,8 +4,9 @@
 $paragrafo = $_GET['paragrafo'];
 $censura = $_GET['badwords'];
 $nuovoParagrafo = str_replace($paragrafo,$censura,'***');
+
 ?>
 
 <p>
-    Il povero <?php $nuovoParagrafo; ?> non sapeva più a chi rivolgersi, ma continuò a saltellare… Lo vide un abete e gli chiese
+    Il povero <?php echo $nuovoParagrafo; ?> non sapeva più a chi rivolgersi, ma continuò a saltellare… Lo vide un abete e gli chiese
 </p>
